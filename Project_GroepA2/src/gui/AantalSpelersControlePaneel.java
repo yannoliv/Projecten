56ae6e1ed0ba2f42c1184a430ac505edfd1bd404
KingNicolas <nicolas.king@users.noreply.github.com>
2018-04-18 09:20:17 +0200

    Aantal spelers paneel gefixt

--- Project_GroepA2/src/gui/AantalSpelersControlePaneel.java
+++ Project_GroepA2/src/gui/AantalSpelersControlePaneel.java
@@ -1,63 +1,51 @@
 package gui;
 
 import domein.DomeinController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
-public class AantalSpelersControlePaneel extends GridPane {
+public class AantalSpelersControlePaneel extends VBox {
     
     private HoofdPaneel hoofdpaneel;
     private DomeinController dc;
     
-    private Label lbl_aantalSpelers;
-    private TextField txt_aantalSpelers;
-    private Label lbl_foutMelding;
+    private final ComboBox cbo_aantalSpelers = new ComboBox();
+    private Button btn_confirm = new Button("Confirm");
+    private Label lbl_foutMelding = new Label("Kies iets anders");
     
-    
-    public AantalSpelersControlePaneel(HoofdPaneel hoofdpaneel)
+    public AantalSpelersControlePaneel(HoofdPaneel hoofdpaneel, DomeinController dc)
     {
         this.hoofdpaneel = hoofdpaneel;
-        setPrefSize(400,400);
-        
-        //Alle items in de scene
-        lbl_aantalSpelers = new Label();
-        lbl_aantalSpelers.setText("Aantal spelers:");
-        lbl_aantalSpelers.setLabelFor(txt_aantalSpelers);
-        GridPane.setRowIndex(lbl_aantalSpelers, 0);
-        GridPane.setColumnIndex(lbl_aantalSpelers, 0);
-        
-        txt_aantalSpelers = new TextField();
-        GridPane.setRowIndex(txt_aantalSpelers, 0);
-        GridPane.setColumnIndex(txt_aantalSpelers, 1);
-        
-        lbl_foutMelding = new Label();
-        lbl_foutMelding.setText("Incorrect");
+        this.dc = dc;
+        setPrefSize(225,250);
+        ObservableList<String> options = FXCollections.observableArrayList("Aantal spelers","2","3","4");
+        cbo_aantalSpelers.setItems(options);
+        cbo_aantalSpelers.getSelectionModel().select(0);
+        VBox.setMargin(cbo_aantalSpelers, new Insets(70, 50, 3, 50));
         lbl_foutMelding.setVisible(false);
-        GridPane.setRowIndex(lbl_foutMelding, 1);
-        GridPane.setColumnIndex(lbl_foutMelding, 1);
-        
-        Button controleerAantalSpelers = new Button("Confirm");
-        GridPane.setRowIndex(controleerAantalSpelers, 1);
-        GridPane.setColumnIndex(controleerAantalSpelers, 0);
-        controleerAantalSpelers.setOnAction(this::controleerAantalSpelers);
-        
-        getChildren().addAll(lbl_aantalSpelers, txt_aantalSpelers, lbl_foutMelding, controleerAantalSpelers);
+        VBox.setMargin(lbl_foutMelding, new Insets(0,0,10,50));
+        VBox.setMargin(btn_confirm, new Insets(0,0,0,75));
+        btn_confirm.setOnAction(this::controleerNamenSpelers);
+        getChildren().addAll(cbo_aantalSpelers, lbl_foutMelding,btn_confirm);
     }
     
-    private void controleerAantalSpelers(ActionEvent event)
+    private void controleerNamenSpelers(ActionEvent event)
     {
-        String antw = txt_aantalSpelers.getText();
-        if (dc.doeAantalSpelersControle(antw) == true)
-        {
-            hoofdpaneel.toonControleNamenSpelers();
+        if (cbo_aantalSpelers.getValue() == cbo_aantalSpelers.getItems().get(0)) {
+            lbl_foutMelding.setVisible(true);
         }
         else
         {
-            lbl_foutMelding.setVisible(true);
+            dc.doeAantalSpelersControle((String) cbo_aantalSpelers.getValue());
+            hoofdpaneel.toonControleNamenSpelers(dc.getSpelerLijst().size());
         }
     }
     
 }
+
--- Project_GroepA2/src/gui/HoofdPaneel.java
+++ Project_GroepA2/src/gui/HoofdPaneel.java
@@ -1,19 +1,24 @@
 package gui;
 
+import domein.DomeinController;
 import javafx.scene.layout.VBox;
 
 
 public class HoofdPaneel extends VBox
 {
-    private AantalSpelersControlePaneel aantalSpelers = new AantalSpelersControlePaneel(this);;
+    private DomeinController dc;
+    private AantalSpelersControlePaneel aantalSpelers;
     
-    public HoofdPaneel(){
+    
+    public HoofdPaneel(DomeinController dc){
+        this.dc = dc;
         setSpacing(10);
+        aantalSpelers = new AantalSpelersControlePaneel(this, dc);
         getChildren().add(aantalSpelers);
     }
     
-    public void toonControleNamenSpelers(){
-        
+    public void toonControleNamenSpelers(int aantal){
+        getChildren().remove(aantalSpelers);
         
     }
 }
