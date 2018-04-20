8e9c83d63affea58270c23d694ac52cc352b22b2
NasRayen <37302314+NasRayen@users.noreply.github.com>
2018-04-20 23:07:39 +0200

    MenuBar code toegevoegd (nog niet zichtbaar)

---
+++ Project_GroepA2/src/gui/MenuBar.java
@@ -0,0 +1,49 @@
+package gui;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.HBox;
+
+public class MenuBar extends HBox {
+    
+    MenuBar menuBar = new MenuBar();
+    
+    
+    public MenuBar()
+    {    
+       javafx.scene.control.MenuBar menuBar = new javafx.scene.control.MenuBar();
+        
+        Menu spelMenu = new Menu("Spel");
+        MenuItem overMenuItem = new MenuItem("Over");
+        MenuItem afsluitenMenuItem = new MenuItem("Afsluiten");
+        afsluitenMenuItem.setAccelerator(KeyCombination.keyCombination("Ctrl+x"));
+
+        overMenuItem.setOnAction(this::aboutClicked);
+        afsluitenMenuItem.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent e)
+            {
+                Platform.exit();
+            }
+        });
+
+        spelMenu.getItems().addAll(overMenuItem, new SeparatorMenuItem(), afsluitenMenuItem);
+        menuBar.getMenus().add(spelMenu);
+     
+    }
+    public void aboutClicked (ActionEvent e) {
+                
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Over");
+        alert.setHeaderText(null);
+        alert.setContentText("Program version 1.0\nMade by King Werner Yannick Rayen");
+        alert.showAndWait();
+    }
+}
--- Project_GroepA2/src/gui/SpelApplicatiePaneel.java
+++ Project_GroepA2/src/gui/SpelApplicatiePaneel.java
@@ -1,11 +1,10 @@
 package gui;
 
 import domein.DomeinController;
+import javafx.geometry.Insets;
+import javafx.scene.control.MenuBar;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.RowConstraints;
 
 
@@ -16,6 +15,7 @@ public class SpelApplicatiePaneel extends GridPane
     private SpelerResourcesPaneel resourcePaneel;
     //panelen
     private MapSpel mapSpel = new MapSpel();
+    private MenuBar menuBar = new MenuBar();
     
 
     public SpelApplicatiePaneel(DomeinController dc)
@@ -27,24 +27,30 @@ public class SpelApplicatiePaneel extends GridPane
 
     private void buildGui() 
     {
-        this.add(mapSpel, 0, 0);
-        this.add(resourcePaneel, 1, 0);
-        //this.add(hutkaartscherm, 0, 1);
-        //this.add(infoscherm, 1, 1)
-
+        this.add(menuBar, 0, 0);
+        this.add(mapSpel, 0, 1);
+        this.add(resourcePaneel, 1, 1);
+        //this.add(hutkaartscherm, 0, 2);
+        //this.add(infoscherm, 1, 2)
+        
+       
         ColumnConstraints column0 = new ColumnConstraints();
         column0.setPercentWidth(70);
         ColumnConstraints column1 = new ColumnConstraints();
         column1.setPercentWidth(30);
         getColumnConstraints().addAll(column0,column1);
+        
+        //.setPadding(new Insets(10, 10, 10, 10));
 
         RowConstraints row0 = new RowConstraints();
-        row0.setPercentHeight(80);
+        row0.setPercentHeight(5);
         RowConstraints row1 = new RowConstraints();
-        row1.setPercentHeight(20);
-        getRowConstraints().addAll(row0,row1);
+        row1.setPercentHeight(75);
+        RowConstraints row2 = new RowConstraints();
+        row2.setPercentHeight(20);
+        getRowConstraints().addAll(row0,row1,row2);
+        
 
-        setGridLinesVisible(true);
+        setGridLinesVisible(true); 
     }
-
-}
+    }
