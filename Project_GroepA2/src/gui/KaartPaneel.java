6f267d717bcfef910e6e728779180a0c978d8c76
rayen.nasra@student.hogent.be <dusklegend1>
2018-04-27 12:44:14 +0200

    Hboxen en vboxen toevoegd

--- Project_GroepA2/src/gui/KaartPaneel.java
+++ Project_GroepA2/src/gui/KaartPaneel.java
@@ -2,6 +2,7 @@ package gui;
 
 import domein.DomeinController;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
@@ -26,6 +27,29 @@ public KaartPaneel(SpelApplicatiePaneel spelApplicatiePaneel, DomeinController d
                 BackgroundPosition.CENTER, achtergrondLengteBreedte);
         this.setBackground(new Background(bgImg));
        // maakKaartPaneel();
+       
+       
+       //main hbox breedte nog instellen
+       HBox box1 = new HBox(
+               new VBox(new ImageView(new Image(getClass().getResourceAsStream("/images/points.gif")))), 
+               new VBox());
+       box1.setStyle("-fx-background-image: url('/images/hutFrame.png');");
+       HBox box2 = new HBox(
+               new VBox(new ImageView(new Image(getClass().getResourceAsStream("/images/points.gif")))), 
+               new VBox());
+       box2.setStyle("-fx-background-image: url('/images/hutFrame.png');");
+       HBox box3 = new HBox(
+               new VBox(new ImageView(new Image(getClass().getResourceAsStream("/images/points.gif")))), 
+               new VBox());
+       box3.setStyle("-fx-background-image: url('/images/hutFrame.png');");
+       HBox hbox = new HBox(
+                   box1, 
+                   new HBox(
+                   box2, 
+                   new HBox(
+                   box3)));
+       this.getChildren().add(hbox);
+       hbox.minWidth(1000);
     }
        
 }
