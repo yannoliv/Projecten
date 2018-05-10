0330c20854bdd2a9c95f3130a51c5e08084d17c5
KingNicolas <nicolas.king@users.noreply.github.com>
2018-05-10 18:01:37 +0200

    nieuwe db

--- db_Project.sql
+++ db_Project.sql
@@ -16,51 +16,50 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `highScores`
+-- Table structure for table `Stenen_Tijdperk`
 --
 
-DROP TABLE IF EXISTS `highScores`;
+DROP TABLE IF EXISTS `Stenen_Tijdperk`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `highScores` (
+CREATE TABLE `Stenen_Tijdperk` (
   `spelerID` int(1) NOT NULL AUTO_INCREMENT,
   `spelerNaam` varchar(45) NOT NULL,
   `rHout` int(1) unsigned zerofill DEFAULT '0',
   `pHout` int(1) unsigned zerofill DEFAULT '0',
-  `rLeem` int(1) DEFAULT '0',
-  `pLeem` int(1) DEFAULT '0',
-  `rSteen` int(1) DEFAULT '0',
-  `pSteen` int(1) DEFAULT '0',
-  `rGoud` int(1) DEFAULT '0',
-  `pGoud` int(1) DEFAULT '0',
-  `rVoedsel` int(1) DEFAULT '12',
-  `pVoedsel` int(1) DEFAULT '0',
-  `rAkkerbouw` int(1) DEFAULT '0',
-  `pAkkerbouw` int(1) DEFAULT '0',
-  `rGereedschap` int(1) DEFAULT '0',
-  `pSmith` int(1) DEFAULT '0',
-  `rStamleden` int(1) DEFAULT '5',
-  `pHut` int(1) DEFAULT '0',
-  `rPunten` int(1) DEFAULT '0',
+  `rLeem` int(1) unsigned zerofill DEFAULT '0',
+  `pLeem` int(1) unsigned zerofill DEFAULT '0',
+  `rSteen` int(1) unsigned zerofill DEFAULT '0',
+  `pSteen` int(1) unsigned zerofill DEFAULT '0',
+  `rGoud` int(1) unsigned zerofill DEFAULT '0',
+  `pGoud` int(1) unsigned zerofill DEFAULT '0',
+  `rVoedsel` int(1) unsigned zerofill DEFAULT '12',
+  `pVoedsel` int(1) unsigned zerofill DEFAULT '0',
+  `rAkkerbouw` int(1) unsigned zerofill DEFAULT '0',
+  `pAkkerbouw` int(1) unsigned zerofill DEFAULT '0',
+  `rGereedschap` int(1) unsigned zerofill DEFAULT '0',
+  `pSmith` int(1) unsigned zerofill DEFAULT '0',
+  `rStamleden` int(1) unsigned zerofill DEFAULT '5',
+  `pHut` int(1) unsigned zerofill DEFAULT '0',
+  `rPunten` int(1) unsigned zerofill DEFAULT '0',
   `hut1` varchar(2) DEFAULT '0',
   `hut2` varchar(2) DEFAULT '0',
   `hut3` varchar(2) DEFAULT '0',
-  `highScore` int(11) DEFAULT NULL,
+  `highScore` int(11) unsigned zerofill DEFAULT NULL,
   PRIMARY KEY (`spelerID`),
-  UNIQUE KEY `spelerNaam_UNIQUE` (`spelerNaam`),
   UNIQUE KEY `spelerId_UNIQUE` (`spelerID`),
   UNIQUE KEY `idx_highScores_spelerId` (`spelerID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `highScores`
+-- Dumping data for table `Stenen_Tijdperk`
 --
 
-LOCK TABLES `highScores` WRITE;
-/*!40000 ALTER TABLE `highScores` DISABLE KEYS */;
-INSERT INTO `highScores` VALUES (1,'Yannick',9,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,'0','0','0',NULL),(3,'Rayen',17,37,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,'0','0','0',NULL);
-/*!40000 ALTER TABLE `highScores` ENABLE KEYS */;
+LOCK TABLES `Stenen_Tijdperk` WRITE;
+/*!40000 ALTER TABLE `Stenen_Tijdperk` DISABLE KEYS */;
+INSERT INTO `Stenen_Tijdperk` VALUES (1,'Yannick',9,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,'0','0','0',NULL),(3,'Rayen',17,37,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,'0','0','0',NULL),(4,'King',0,0,0,0,0,0,0,0,12,0,0,0,0,0,5,0,150,'0','0','0',NULL),(5,'Roberto',0,0,0,0,0,0,0,0,12,0,0,0,0,0,5,0,111,'0','0','0',NULL),(6,'Juliano',0,0,0,0,0,0,0,0,12,0,0,0,0,0,5,0,50,'0','0','0',NULL),(9,'King',0,0,0,0,0,0,0,0,12,0,0,0,0,0,5,0,70,'0','0','0',NULL),(13,'Juliano',0,0,0,0,0,0,0,0,12,0,0,0,0,0,5,0,50,'0','0','0',NULL),(14,'King',0,0,0,0,0,0,0,0,12,0,0,0,0,0,5,0,70,'0','0','0',NULL),(15,'Roberto',0,0,0,0,0,0,0,0,12,0,0,0,0,0,5,0,80,'0','0','0',NULL),(16,'Joseph',0,0,0,0,0,0,0,0,12,0,0,0,0,0,5,0,190,'0','0','0',NULL),(17,'Padre',0,0,0,0,0,0,0,0,12,0,0,0,0,0,5,0,220,'0','0','0',NULL);
+/*!40000 ALTER TABLE `Stenen_Tijdperk` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -72,4 +71,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-09 10:34:39
+-- Dump completed on 2018-05-10 17:59:49
