2119b1152f084523220930d7d61d8b1356c1ebb5
yannoliv <yannoliv@student.arteveldehs.be>
2018-05-11 14:52:28 +0200

    buttonHome

--- Project_GroepA2/src/gui/MainMenu.java
+++ Project_GroepA2/src/gui/MainMenu.java
@@ -8,12 +8,12 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import mapper.SpelerMapper;
 
 public class MainMenu extends VBox {
     
     private DomeinController dc;
     private Stage stage;
+    private Button btn_loadGame;
     private Button btn_start;
     private Button btn_scorebord;
 //    private SpelerMapper spelerMapper;
@@ -23,38 +23,44 @@ public class MainMenu extends VBox {
     {
         this.dc = dc;
         this.stage = stage;
-//        this.spelerMapper = new SpelerMapper();
-        maakMainMenu();       
+        this.maakMainMenu();
+    }
+    
+    private void loadGame(ActionEvent ae)
+    {
+        
     }
     
     private void startSpel(ActionEvent ae)
     {
-        getChildren().removeAll(btn_start, btn_scorebord);
+        getChildren().removeAll(btn_loadGame, btn_start, btn_scorebord);
         KeuzePaneel st = new KeuzePaneel(dc, stage, this);
         getChildren().add(st);
     }
+    
     private void scorebord(ActionEvent ae)
     {
-       
-        getChildren().removeAll(btn_start, btn_scorebord);
-      
+        getChildren().removeAll(btn_loadGame, btn_start, btn_scorebord);
         ScoreBord sb = new ScoreBord(dc, stage, this);
         getChildren().add(sb);
-        
-
     }
     
     public void maakMainMenu()
     {
-        btn_start = new Button("Start");
+        btn_loadGame = new Button("Laad spel");
+        btn_start = new Button("Start nieuw spel");
         btn_scorebord = new Button("Scorebord");
+        btn_loadGame.setMinWidth(250);
         btn_start.setMinWidth(250);
         btn_scorebord.setMinWidth(250);
+        btn_loadGame.setMinHeight(50);
         btn_start.setMinHeight(50);
         btn_scorebord.setMinHeight(50);
         this.setAlignment(Pos.CENTER);
-        VBox.setMargin(btn_start, new Insets(100,0,25,0));
-        getChildren().addAll(btn_start, btn_scorebord);
+        VBox.setMargin(btn_start, new Insets(20,0,40,0));
+        //VBox.setMargin(btn_scorebord, new Insets(20,0,20,0));
+        getChildren().addAll(btn_loadGame,btn_start, btn_scorebord);
+        btn_loadGame.setOnAction(this::loadGame);
         btn_start.setOnAction(this::startSpel);
         btn_scorebord.setOnAction(this::scorebord);
         stage.setTitle("Stenen Tijdperk");
--- Project_groepA2/src/gui/ScoreBord.java
+++ Project_groepA2/src/gui/ScoreBord.java
@@ -1,6 +1,8 @@
 package gui;
 
 import domein.DomeinController;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -14,6 +16,7 @@ public class ScoreBord extends VBox
     private MainMenu mainMenu;
     private Label lbl_label;
     private SpelerMapper spelerMapper;
+    private Button btn_home;
     
     public ScoreBord(DomeinController dc, Stage stage, MainMenu mainMenu) 
     {
@@ -22,9 +25,13 @@ public class ScoreBord extends VBox
         this.mainMenu = mainMenu;
         this.spelerMapper = new SpelerMapper();
         
+        btn_home = new Button("Home");
+        btn_home.setMinWidth(250);
+        btn_home.setMinHeight(50);
+        
         lbl_label = new Label("HighScores");
         lbl_label.setStyle("-fx-font-size:4em; -fx-padding:15px;");
-        this.getChildren().add(lbl_label);
+        this.getChildren().addAll(btn_home,lbl_label);
         
         for (int i = 0; i < 10; i++) {
             lbl_label = new Label(String.format("%-5d  %-10s   %30d", spelerMapper.geefHighScores(dc).get(i).getSpelerNummer() + 1, spelerMapper.geefHighScores(dc).get(i).getNaam(), spelerMapper.geefHighScores(dc).get(i).getTotaalScore()));
@@ -33,6 +40,15 @@ public class ScoreBord extends VBox
         }
         this.setStyle("-fx-fill:rgb(255,255,255);-fx-alignment: center;-fx-background-color:rgba(255,255,255,0.7);");
         
-}
+        btn_home.setOnAction(this::home);
+    }
+    
+    public void home(ActionEvent ae)
+    {
+        this.setStyle(null);
+        getChildren().clear();
+        MainMenu mm = new MainMenu(dc, stage);
+        getChildren().add(mm);
+    }
     
 }
