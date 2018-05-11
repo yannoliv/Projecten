0aa658d6345b511cefd96c617ddbe404e573ea00
yannoliv <yannoliv@student.arteveldehs.be>
2018-05-11 16:56:29 +0200

    buttonextra

--- Project_GroepA2/src/gui/MainMenu.java
+++ Project_GroepA2/src/gui/MainMenu.java
@@ -16,6 +16,7 @@ public class MainMenu extends VBox {
     private Button btn_loadGame;
     private Button btn_start;
     private Button btn_scorebord;
+    private Button btn_demo;
 //    private SpelerMapper spelerMapper;
    
     
@@ -33,16 +34,21 @@ public class MainMenu extends VBox {
     
     private void startSpel(ActionEvent ae)
     {
-        getChildren().removeAll(btn_loadGame, btn_start, btn_scorebord);
+        getChildren().removeAll(btn_loadGame, btn_start, btn_scorebord, btn_demo);
         KeuzePaneel st = new KeuzePaneel(dc, stage, this);
         getChildren().add(st);
     }
     
     private void scorebord(ActionEvent ae)
     {
-        getChildren().removeAll(btn_loadGame, btn_start, btn_scorebord);
+        getChildren().removeAll(btn_loadGame, btn_start, btn_scorebord, btn_demo);
         ScoreBord sb = new ScoreBord(dc, stage, this);
         getChildren().add(sb);
+    }
+    
+    private void demo(ActionEvent ae)
+    {
+        
     }
     
     public void maakMainMenu()
@@ -50,19 +56,23 @@ public class MainMenu extends VBox {
         btn_loadGame = new Button("Laad spel");
         btn_start = new Button("Start nieuw spel");
         btn_scorebord = new Button("Scorebord");
+        btn_demo = new Button("Start demo");
         btn_loadGame.setMinWidth(250);
         btn_start.setMinWidth(250);
         btn_scorebord.setMinWidth(250);
+        btn_demo.setMinWidth(250);
         btn_loadGame.setMinHeight(50);
         btn_start.setMinHeight(50);
         btn_scorebord.setMinHeight(50);
+        btn_demo.setMinHeight(50);
         this.setAlignment(Pos.CENTER);
-        VBox.setMargin(btn_start, new Insets(20,0,40,0));
-        //VBox.setMargin(btn_scorebord, new Insets(20,0,20,0));
-        getChildren().addAll(btn_loadGame,btn_start, btn_scorebord);
+        VBox.setMargin(btn_start, new Insets(20,0,20,0));
+        VBox.setMargin(btn_scorebord, new Insets(20,0,20,0));
+        getChildren().addAll(btn_loadGame,btn_start, btn_scorebord, btn_demo);
         btn_loadGame.setOnAction(this::loadGame);
         btn_start.setOnAction(this::startSpel);
         btn_scorebord.setOnAction(this::scorebord);
+        btn_demo.setOnAction(this::demo);
         stage.setTitle("Stenen Tijdperk");
         stage.setWidth(1280);
         stage.setHeight(720);
